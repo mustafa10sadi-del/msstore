9d2e48f5e4c422b75d6c14c6abc2818aaea04251
msstore95 <mustafa10sadi@gmail.com>
2026-05-03 03:14:55 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,20 +2,27 @@ importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js");
 
 firebase.initializeApp({
-  apiKey: "AIzaSyDakH4F8s6tmdSVTFENXMInK5oQABPSSVo",
+  apiKey: "AIzaSyDakH4F856tmdSVTFENXMINk5oQABPSSVo",
   authDomain: "msstore-5c5f4.firebaseapp.com",
   databaseURL: "https://msstore-5c5f4-default-rtdb.firebaseio.com",
   projectId: "msstore-5c5f4",
   storageBucket: "msstore-5c5f4.appspot.com",
-  messagingSenderId: "254756613602",
-  appId: "1:254756613602:web:35825d0b88c3dae8545dbb"
+  messagingSenderId: "25475613602",
+  appId: "1:25475613602:web:35825d0b88c3dae8545dbb"
 });
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function(payload) {
-  const title = payload.notification?.title || "متجر MS";
-  const body = payload.notification?.body || "وصل إشعار جديد";
+  const title =
+    payload.notification?.title ||
+    payload.data?.title ||
+    "إشعار جديد";
+
+  const body =
+    payload.notification?.body ||
+    payload.data?.body ||
+    "وصل إشعار جديد";
 
   self.registration.showNotification(title, {
     body: body,
