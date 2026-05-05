30654847afdc7d1c310486a9c2086007ff404427
msstore95 <mustafa10sadi@gmail.com>
2026-05-05 14:04:15 +0300

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -4,17 +4,6 @@ import App from "./App";
 import "./index.css";
 import "./firebase-notifications";
 
-// تسجيل Service Worker للإشعارات
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./firebase-messaging-sw.js')
-    .then((registration) => {
-      console.log('تم تسجيل الإشعارات ✅', registration);
-    })
-    .catch((err) => {
-      console.log('فشل تسجيل الإشعارات ❌', err);
-    });
-}
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
